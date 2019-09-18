bcc5fe09f33db1e71c8115e9f3a20f337d834ba8
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-09-18 23:14:40 +0300

    reading the audio file module

--- app/src/main/java/ke/co/davidwanjohi/audioreader/MainActivity.java
+++ app/src/main/java/ke/co/davidwanjohi/audioreader/MainActivity.java
@@ -1,14 +1,101 @@
 package ke.co.davidwanjohi.audioreader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 
+import android.Manifest;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    AppCompatTextView audioFileName;
+    AppCompatButton audioFileBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        audioFileName=(AppCompatTextView) findViewById(R.id.selected_audio_file);
+        audioFileBtn=(AppCompatButton) findViewById(R.id.select_btn);
+
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+//                getAllSongsFromSDCARD();
+
+
+            }
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
+        }).withErrorListener(new PermissionRequestErrorListener() {
+                    @Override public void onError(DexterError error) {
+                        Log.e("Dexter", "There was an error: " + error.toString());
+                    }
+                }).check();
+
+
+
     }
+
+    public void getAllSongsFromSDCARD()
+    {
+        String[] STAR = { "*" };  //it is projection you can modify it according to your need
+        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+        String[] selectionArgs = null;
+        Uri allsongsuri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";  // check for music files
+
+     Cursor cursor = getContentResolver().query(allsongsuri,STAR,selection,selectionArgs,sortOrder );
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    String song_name = cursor
+                            .getString(cursor
+                                    .getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    int song_id = cursor.getInt(cursor
+                            .getColumnIndex(MediaStore.Audio.Media._ID));
+
+                    String fullpath = cursor.getString(cursor
+                            .getColumnIndex(MediaStore.Audio.Media.DATA));
+
+
+                    String album_name = cursor.getString(cursor
+                            .getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                    int album_id = cursor.getInt(cursor
+                            .getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
+
+                    String artist_name = cursor.getString(cursor
+                            .getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                    int artist_id = cursor.getInt(cursor
+                            .getColumnIndex(MediaStore.Audio.Media.ARTIST_ID));
+
+
+
+                } while (cursor.moveToNext());
+
+            }
+            cursor.close();
+        }
+    }
+
 }
